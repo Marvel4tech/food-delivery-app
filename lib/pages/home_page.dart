975653485622109be046cb707c589c5b554c3603
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2025-08-14 20:14:26 +0100

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/components/my_current_location.dart';
+import 'package:my_app/components/my_description_box.dart';
 import 'package:my_app/components/my_drawer.dart';
+import 'package:my_app/components/my_silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +15,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
       drawer: MyDrawer(),
+
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: Text("title"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+
+                // my current location
+                const MyCurrentLocation(),
+
+                // description box
+                const MyDescriptionBox(),
+              ],
+            ),
+          )
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
